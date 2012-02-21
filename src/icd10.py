925ead281e881366409183fdb52dce4ddc060b02
Even Wiik Thomassen <eventh@gmail.com>
2012-02-21 15:35:41 +0100

    Finished converting to json.

--- src/icd10.py
+++ src/icd10.py
@@ -1,6 +1,7 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import os
 
 try:
     import xml.etree.cElementTree as ET
@@ -13,16 +14,77 @@ except ImportError:
     import json
 
 
+class ICD10(object):
+
+    def __init__(self):
+        self.inclusion = []
+        self.exclusion = []
+        self.terms = []
+        self.synonym = []
+
+        self.short = None
+        self.code = None
+        self.label = None
+        self.formatted = None
+        self.type = None
+        self.icpc2_label = None
+
+    def __str__(self):
+        output = "%s: %s" % (self.short, self.label)
+        return output.encode('ascii', 'ignore')
+
+
 def parse_xml_file(path):
     tree = ET.parse(path)
     return tree.getroot()
 
 
+def simplify_tag(tag):
+    return tag.split('}')[1]
+
+
 def traverse_tree(root):
+    objects = []
     nodes = root.findall('{http://www.w3.org/2002/07/owl#}Class')
-    print "Nodes:", len(nodes)
+
     for node in nodes:
-        print node.attrib
+        obj = ICD10()
+        for child in node:
+            tag = simplify_tag(child.tag)
+
+            if tag == 'label':
+                obj.label = child.text
+            elif tag == 'code_compacted':
+                obj.short = child.text
+            elif tag == 'code_formatted':
+                obj.formatted = child.text
+            elif tag == 'subClassOf':
+                pass
+            elif tag == 'umls_atomId':
+                pass
+            elif tag == 'umls_conceptId':
+                pass
+            elif tag == 'umls_tui':
+                pass
+            elif tag == 'umls_semanticType':
+                obj.type = child.text
+            elif tag == 'icpc2_code':
+                obj.code = child.text
+            elif tag == 'icpc2_label':
+                obj.icpc2_label = child.text
+            elif tag == 'underterm':
+                obj.terms.append(child.text.strip())
+            elif tag == 'synonym':
+                obj.synonym.append(child.text.strip())
+            elif tag == 'inclusion':
+                obj.inclusion.append(child.text)
+            elif tag == 'exclusion':
+                obj.exclusion.append(child.text)
+            else:
+                print simplify_tag(child.tag), ":", child.text, child.tail
+
+        objects.append(obj)
+    return objects
 
 
 def main(script, path=None):
@@ -31,7 +93,12 @@ def main(script, path=None):
         sys.exit(2)
 
     root = parse_xml_file(path)
-    traverse_tree(root)
+    objects = traverse_tree(root)
+
+    folder, filename = os.path.split(path)
+    filename, ext = os.path.splitext(filename)
+    with open("%s/%s.json" % (folder, filename), 'w') as f:
+        json.dump(objects, f)
 
     sys.exit(None)
 
