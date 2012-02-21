6c9dc2d0e070f841a31aae39e1de150c3fe7c8a9
Even Wiik Thomassen <eventh@gmail.com>
2012-02-21 20:57:38 +0100

    Now we can search on label in ICD10 index :)

--- src/search.py
+++ src/search.py
@@ -11,25 +11,29 @@ from whoosh.qparser import QueryParser
 from schemas import ICD10_SCHEMA, INDEX_DIR
 
 
+def print_result(result):
+    print result
+    for res in result:
+        print res
+
+
 def search_icd10(query):
     ix = open_dir(INDEX_DIR, indexname='icd10')
     with ix.searcher() as searcher:
         qp = QueryParser('label', schema=ix.schema)
-        q = qp.parse(u'test')
-        result = searcher.search(q)
-    return result
+        q = qp.parse(query)
+        print_result(searcher.search(q))
 
 
 def main(script, index='', *query):
+    flat_query = unicode(''.join(query))  # TODO
+
     if index == 'icd10':
-        results = search_icd10(query)
+        search_icd10(flat_query)
 
     else:
         print "Unknown database: %s" % index
         sys.exit(2)
-
-    # Print results
-    print results
 
 
 if __name__ == '__main__':
