a20ef3fa7ea6aca74445832162cb1a89e4e795ac
Even Wiik Thomassen <eventh@gmail.com>
2012-02-21 09:00:32 +0100

    Started working on file to parse the ircd10no xml file.

---
+++ src/icd10.py
@@ -0,0 +1,37 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+
+def parse_xml_file(path):
+    tree = ET.parse(path)
+    return tree.getroot()
+
+
+def traverse_tree(root):
+    print root
+
+
+def main(script, path=None):
+    if path is None:
+        print "Need to supply icd10 file or json file to parse"
+        sys.exit(2)
+
+    root = parse_xml_file(path)
+    traverse_tree(root)
+
+    sys.exit(None)
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
