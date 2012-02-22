2fd3f4f05929ceaf5f71113c7da5a3b59b7ea3f8
Even Wiik Thomassen <eventh@gmail.com>
2012-02-22 16:06:07 +0100

    Fixed cases icd10 search to use OR instead of AND.

--- src/cases.py
+++ src/cases.py
@@ -7,9 +7,13 @@ import sys
 import os
 from operator import itemgetter
 
-from search import search_icd10
+from whoosh.index import open_dir
+from whoosh.qparser import QueryParser, OrGroup
+
+from schemas import ICD10_SCHEMA, INDEX_DIR
 
 
+# Folder where tasks are stored as .txt files.
 TASK_DIR = 'etc/'
 
 
@@ -20,24 +24,29 @@ def read_cases_from_files(folder):
         filename, ext = os.path.splitext(path)
         if not os.path.isdir(full_path) and ext == '.txt':
             with open(full_path) as f:
-                lines = [unicode(i, errors='ignore') for i in f.readlines()]
+                lines = [unicode(i, errors='ignore')
+                            for i in f.readlines() if i]
             cases[filename.replace('case', '')] = lines
     return cases
 
 
 def icd10_case_search(cases):
-    def result_handler(result):
-        return len(result)
+    ix = open_dir(INDEX_DIR, indexname='icd10')
+    qp = QueryParser('label', schema=ix.schema, group=OrGroup)
 
-    print "Case:Line - Results"
-    for case, lines in sorted(cases.items(), key=itemgetter(0)):
-        i = 0
-        for query in lines:
-            if not query:
-                continue
-            i += 1
-            results = search_icd10(query, result_handler)
-            print "%s:%i - %i" % (case, i, results)
+    with ix.searcher() as searcher:
+
+        print "Case:Line - Results"
+        for case, lines in sorted(cases.items(), key=itemgetter(0)):
+
+            i = 0
+            for line in lines:
+                q = qp.parse(line)
+                results = searcher.search(q)
+
+                i += 1
+                print "%s:%i - %i" % (case, i, 0)
+                print results[0:2]
 
 
 def main(script):
