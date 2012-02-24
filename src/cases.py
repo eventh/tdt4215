6cb1c978c8dfc7e1011d501e8d6dc08907c73849
Even Wiik Thomassen <eventh@gmail.com>
2012-02-24 18:53:05 +0100

    Fixed handling of cases in cases.py. Now can run all or a specific path.

--- src/cases.py
+++ src/cases.py
@@ -13,24 +13,31 @@ from whoosh.qparser import QueryParser, OrGroup
 from schemas import ATC_SCHEMA, ICD10_SCHEMA, INDEX_DIR
 
 
-# Folder where tasks are stored as .txt files.
-TASK_DIR = 'etc/'
+def read_cases_from_files(folder_or_path):
+    # Find case paths if path is a folder
+    paths = []
+    if not os.path.isdir(folder_or_path):
+        paths.append(folder_or_path)
+    else:
+        for path in os.listdir(folder_or_path):
+            full_path = os.path.normpath(os.path.join(folder_or_path, path))
+            if not os.path.isdir(full_path):
+                paths.append(full_path)
 
-
-def read_cases_from_files(folder):
+    # Read in lines from case files
     cases = {}
-    for path in os.listdir(folder):
-        full_path = os.path.normpath(os.path.join(folder, path))
-        filename, ext = os.path.splitext(path)
-        if (not os.path.isdir(full_path) and
-                ext == '.txt' and filename.startswith('case')):
-            with open(full_path) as f:
+    for path in paths:
+        filename, ext = os.path.splitext(os.path.split(path)[1])
+        if ext == '.txt' and filename.startswith('case'):
+            with open(path) as f:
                 lines = [i.strip() for i in f.readlines()]
-            cases[filename.replace('case', '')] = [i for i in lines if i]
+                cases[filename] = [i for i in lines if i]
+
     return cases
 
 
-def icd10_case_search(cases):
+def task_1a(cases):
+    """Perform task 1 A. Search through ICD10 codes."""
     ix = open_dir(INDEX_DIR, indexname='icd10')
     qp = QueryParser('label', schema=ix.schema, group=OrGroup)
 
@@ -49,11 +56,33 @@ def icd10_case_search(cases):
                 print(results[0:2])
 
 
-def main(script, case='', task='', output=''):
-    cases = read_cases_from_files(TASK_DIR)
-    print("Loaded '%s' cases from '%s'" % (len(cases), TASK_DIR))
+def task_2(cases):
+    """Perform task 2. Search through ATC codes."""
+    pass
 
-    icd10_case_search(cases)
+
+TASKS = {'1a': task_1a, '2': task_2}
+
+
+def main(script, task='', case='', output=''):
+    """Perform project tasks on cases.
+
+    'task' is the project task to run, optional.
+    'case' is a path to the case to run, optional.
+    'output' is the output to generate, optional.
+    Usage: 'python3 cases.py [task] [case] [latex|json]'.
+    """
+    # Handle cases
+    if '.txt' in task:
+        output = case
+        case = task
+        task = ''
+    if not case:
+        case = 'etc/'
+    cases = read_cases_from_files(case)
+    print("Loaded '%s' cases from '%s'" % (len(cases), case))
+
+    #icd10_case_search(cases)
 
     sys.exit(None)
 
