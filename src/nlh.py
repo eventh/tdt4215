7ca846431ec6c35ba75f6b06a9059be3ffd0925d
Even Wiik Thomassen <eventh@gmail.com>
2012-02-25 14:24:48 +0100

    Added a python module for parsing legemiddelhåndboka.
    Haven't yet decided how we want to do this.

---
+++ src/nlh.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module for parsing html documents from 'norsk legemiddelhåndbok'.
+"""
+import sys
+from html.parser import HTMLParser
+
+
+def main(script):
+    sys.exit(None)
+
+
+if __name__ == '__main__':
+    main(*sys.argv)
